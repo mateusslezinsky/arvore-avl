57768e54b3aedc55e115be707d00fc2ddb941398
Mateus Pereira <mateus.sle.per@hotmail.com>
2023-05-23 10:03:27 -0300

    Basic initial implementation

---
+++ main.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+
+class ArvoreAVL:
+    def __init__(self):
+        pass
